335c112c5f9ccf520785c9e2b00fbe4db40a026d
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2018-06-23 18:02:25 +0200

    migracja działa

--- DB.php
+++ DB.php
@@ -2,18 +2,15 @@
 
 namespace Core;
 
-class DB {
-
+class DB
+{
+    /**
+     * @var \PDO
+     */
     private static $pdo = null;
 
-    static function connect() {
-        if (static::$pdo === null) {
-            static::$pdo = new \PDO(getenv('db'), getenv('dbUser'), getenv('dbPass'));
-            static::$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        }
-    }
-
-    static function get(string $sql, $params = []) {
+    static function get(string $sql, $params = [])
+    {
         static::connect();
         $sth = static::$pdo->prepare($sql, array(\PDO::ATTR_CURSOR => \PDO::CURSOR_FWDONLY));
         $params2 = [];
@@ -25,24 +22,58 @@ class DB {
         return $ret;
     }
 
-    static function rollBack() {
+    static function connect()
+    {
+        if (static::$pdo === null) {
+            static::$pdo = new \PDO(getenv('db'), getenv('dbUser'), getenv('dbPass'));
+            static::$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        }
+    }
+
+    static function query(string $sql, $params = [])
+    {
+        static::connect();
+        $sth = static::$pdo->prepare($sql, array(\PDO::ATTR_CURSOR => \PDO::CURSOR_FWDONLY));
+        $params2 = [];
+        foreach ($params as $name => $value) {
+            $params2[':'.$name] = $value;
+        }
+        $sth->execute($params);
+    }
+
+    static function rollBack()
+    {
         static::connect();
         static::$pdo->rollback();
     }
 
-    static function commit() {
+    static function commit()
+    {
         static::connect();
         static::$pdo->commit();
     }
 
-    static function beginTransaction() {
+    static function beginTransaction()
+    {
         static::connect();
         static::$pdo->beginTransaction();
     }
 
-    static function lastInsertId() {
+    static function lastInsertId()
+    {
         static::connect();
         return static::$pdo->lastInsertId();
+    }
+
+    static function safe($val)
+    {
+        static::connect();
+        if ($val === NULL)
+            return null;
+        if (is_int($val))
+            return (int)$val;
+        return "'".static::$pdo->quote($val)."'";
+
     }
 
 }
--- Migration.php
+++ Migration.php
@@ -2,43 +2,98 @@
 
 namespace Core;
 
-class Migration {
-    function readNewStructure(){
-        $tables=[];
-         $modules = scandir(__DIR__.'/../');
+class Migration
+{
+    function upgrade()
+    {
+        $old = $this->readOldStructure();
+        $new = $this->readNewStructure();
+        foreach ($new as $name => $tableNew) {
+            if (isset($old[$name])) {
+                dump($tableNew);
+                $sqls = [];
+                foreach ($tableNew->column as $colNew) {
+                    $oldCol = null;
+                    foreach ($old[$name] as $oldtableCol) {
+                        dump($oldtableCol['Field'],$colNew->name);
+                        if ($oldtableCol['Field'] == $colNew->name->__toString()) {
+                            $oldCol = $oldtableCol;
+                            break;
+                        }
+                    }
+                    dump($colNew, $oldCol);
+                    if ($oldCol) {
+                        $sqls[] = 'CHANGE `'.$colNew->name.'` '.$this->createColumnSql($colNew);
+
+                    } else {
+                        $sqls[] = 'ADD '.$this->createColumnSql($colNew);
+                    }
+
+                }
+                $sqlsString = implode(',', $sqls);
+                $sql = "ALTER TABLE `$name` $sqlsString";
+                dump($sql);
+                DB::query($sql);
+            } else {
+                $this->createTable($name, $tableNew);
+            }
+        }
+    }
+
+    function readOldStructure()
+    {
+        $tablesList = DB::get("SHOW TABLES");
+        $tables = [];
+        foreach ($tablesList as $table) {
+            $tables[$table[0]] = DB::get("SHOW COLUMNS FROM ".$table[0]);
+        }
+        return $tables;
+    }
+
+    function readNewStructure()
+    {
+        $tables = [];
+        $modules = scandir(__DIR__.'/../');
         foreach ($modules as $module) {
             if ($module == '.' || $module == '..') {
                 continue;
             }
             $filename = __DIR__.'/../'.$module.'/db.xml';
             if (is_file($filename)) {
-                $xml=simplexml_load_string(file_get_contents($filename));
-                foreach ($xml->table as $table){
-                    dump($table);
-                    dump($table->name->__toString());
-                    $tables[$table->name->__toString()]=$table;
+                $xml = simplexml_load_string(file_get_contents($filename));
+                foreach ($xml->table as $table) {
+                    $tables[$table->name->__toString()] = $table;
                 }
             }
         }
         return $tables;
     }
-    function readOldStructure(){
-        $tablesList=DB::get("SHOW TABLES");
-        $tables=[];
-        foreach($tablesList as $table){
-            $tables[$table[0]]=DB::get("SHOW COLUMNS FROM ".$table[0]);
-        }
-        return $tables;
+
+    /**
+     * @param $column
+     * @return string
+     */
+    protected function createColumnSql($column)
+    {
+        $col = '`'.$column->name.'` '.$column->type.' '.($column->null == 'YES' ? 'NULL' : 'NOT NULL');
+        if (!empty($column->default))
+            $col .= ' DEFAULT '.DB::safe($column->default->__toString());
+        return $col;
     }
-    function upgrade(){
-        $old=$this->readOldStructure();
-        $new=$this->readNewStructure();
-        foreach($new as $name=>$table){
-            if(isset($old[$name])){
-                
-            }else{
-              
-            }
+
+    /**
+     * @param $name
+     * @param $content
+     */
+    protected function createTable($name, $content)
+    {
+        $cols = [];
+        foreach ($content->column as $column) {
+            $cols[] = $this->createColumnSql($column);
         }
+        $colsString = implode(',', $cols);
+        $sql = "CREATE TABLE `$name`($colsString)";
+        dump($sql);
+        DB::query($sql);
     }
 }
