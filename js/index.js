fb71d365a2322b48b4ed962e988c70aee05ca307
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2018-07-24 11:49:36 +0200

    PWA

--- js/index.js
+++ js/index.js
@@ -27,5 +27,16 @@ pageManager.onLoad(async (page, data) => {
 addEventListener('focus', () => {
     AjaxTask.refresh();
 });
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
 
 pageManager.initPage(window.controllerInitInfo);
