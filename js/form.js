26936474013cef44bdcb4248eb079555e4b6e867
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2018-10-02 19:46:26 +0200

    poprawa fromularzy wielopoziomowych

--- js/form.js
+++ js/form.js
@@ -4,40 +4,58 @@ export const formManager = {
     load(form, data) {
         let formElements = form.elements;
         for (var elem of formElements) {
-            if (data[elem.name]) {
-                elem.value = data[elem.name];
+            let value = this.parseFormItemNameRead(elem, data);
+            if (elem.type == 'checkbox') {
+                elem.checked = value
             } else {
-                elem.value = '';
+                elem.value = value;
+
             }
         }
     },
     initEvents() {
         document.querySelectorAll('form.dataForm').forEach(form => form.addEventListener('submit', event => this.formSubmitted(event, form)));
     },
-    formSubmitted(e, form) {
+    parseFormItemNameWrite: function (elem, data, value) {
+        let nameParsed = /^([^\[]+)\[?/.exec(elem.name);
+        if (!nameParsed)
+            return null;
+        let obj = data;
+        let nameLeft = elem.name.replace(/^[^\]]+\[/, '[');
+        while (/^\[[^\]]*\]/.test(nameLeft)) {
+            if (!(obj[nameParsed[1]] instanceof Object)) {
+                obj[nameParsed[1]] = {};
+            }
+            obj = obj[nameParsed[1]];
+            nameParsed = /^\[([^\]]*)\]/.exec(nameLeft);
+            nameLeft = nameLeft.replace(/^\[[^\]]*\]/, '');
+        }
+        obj[nameParsed[1]] = elem.value;
+    },
+    parseFormItemNameRead: function (elem, data, value) {
+        let nameParsed = /^([^\[]+)\[?/.exec(elem.name);
+        if (!nameParsed)
+            return null;
+        let obj = data;
+        let nameLeft = elem.name.replace(/^[^\]]+\[/, '[');
+        while (/^\[[^\]]*\]/.test(nameLeft)) {
+            if (!(obj[nameParsed[1]] instanceof Object)) {
+                return null
+            }
+            obj = obj[nameParsed[1]];
+            nameParsed = /^\[([^\]]*)\]/.exec(nameLeft);
+            nameLeft = nameLeft.replace(/^\[[^\]]*\]/, '');
+        }
+        return obj[nameParsed[1]];
+    }, formSubmitted(e, form) {
         var data = {};
         let formElements = form.elements;
         for (var elem of formElements) {
-            if(elem.type=='checkbox'){
-                if(!elem.checked)
+            if (elem.type == 'checkbox') {
+                if (!elem.checked)
                     continue;
             }
-            let nameParsed = /^([^\]]+)\[/.exec(elem.name);
-            if (nameParsed) {
-                let obj = data;
-                let nameLeft = elem.name.replace(/^[^\]]+\[/, '[');
-                while (/^\[[^\]]*\]/.test(nameLeft)) {
-                    if (!(obj[nameParsed[1]] instanceof Object)) {
-                        obj[nameParsed[1]] = {};
-                    }
-                    obj = obj[nameParsed[1]];
-                    nameParsed = /^\[([^\]]*)\]/.exec(nameLeft);
-                    nameLeft = nameLeft.replace(/^\[[^\]]*\]/, '');
-                }
-                obj[nameParsed[1]] = elem.value;
-            }
-            else
-                data[elem.name] = elem.value;
+            this.parseFormItemNameWrite(elem, data, elem.value);
         }
         let task = new AjaxTask();
         task.newTask(form.dataset.controller, form.dataset.method, data);
