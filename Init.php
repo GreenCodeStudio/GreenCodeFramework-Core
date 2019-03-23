a9aebe14e4d9f71588c8e59109048d203cd1802d
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2019-03-23 12:12:44 +0100

    linux

--- Init.php
+++ Init.php
@@ -2,7 +2,7 @@
 
 error_reporting(E_ALL);
 spl_autoload_register(function ($class_name) {
-    include __DIR__.'/../'.$class_name.'.php';
+    include __DIR__.'/../'.str_replace("\\","/", $class_name).'.php';
 });
 global $debugType;
 $debugType = 'html';
