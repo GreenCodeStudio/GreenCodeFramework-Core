e4eef3e3c033ec8de70ec1238fce14eb2ac5fd0f
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2019-06-01 10:04:03 +0200

    pagination

--- js/table.js
+++ js/table.js
@@ -23,7 +23,9 @@ export class TableManager {
     /**
      * virtual method
      */
-    calcSize(){}
+    calcSize() {
+    }
+
     loadData(data) {
         let tbody = this.table.tBodies[0];
         if (!tbody) {
@@ -47,5 +49,73 @@ export class TableManager {
                 }
             }
         }
+        if (!this.table.tFoot)
+            this.table.tFoot = document.create('tfoot');
+        this.renderPagination(data.total);
+    }
+
+    goToPage(pageNumber) {
+        this.page = pageNumber;
+        this.refresh();
+    }
+
+    renderPagination(totalRows) {
+        let pagination = this.getPagination(totalRows);
+        console.log({pagination});
+        if (!this.paginationRow) {
+            this.paginationRow = this.table.tFoot.addChild('tr').addChild('td');
+        }
+        this.paginationRow.colSpan = this.table.tHead.firstElementChild.children.length;
+        this.paginationRow.children.removeAll();
+        for (let pageNumber of pagination) {
+            if (pageNumber == null) {
+                this.paginationRow.addChild('span', {text: '...'});
+            } else {
+                let pageButton = this.paginationRow.addChild('button', {text: pageNumber + 1});
+                pageButton.onclick = () => {
+                    this.goToPage(pageNumber);
+                };
+            }
+        }
+    }
+
+    getPagination(totalRows) {
+        const boxesCount = 7;
+        let pages = Math.ceil(totalRows / this.limit);
+        let boxes = [];
+        if (pages <= boxesCount) {
+            for (let i = 0; i < pages; i++) {
+                boxes.push(i);
+            }
+        } else if (this.page <= (boxesCount - 1) / 2 - 1) {
+            for (let i = 0; i <= boxesCount - 3; i++) {
+                boxes.push(i);
+            }
+            boxes.push(null);
+            boxes.push(pages - 1);
+        } else if (this.page >= pages - (boxesCount - 1) / 2 - 1) {
+            boxes.push(0);
+            boxes.push(null);
+            for (
+                let i = pages - Math.floor((boxesCount - 1) / 2) - 2;
+                i < pages;
+                i++
+            ) {
+                boxes.push(i);
+            }
+        } else {
+            boxes.push(0);
+            boxes.push(null);
+            for (
+                let i = this.page - Math.floor((boxesCount - 5) / 2);
+                i <= this.page + Math.ceil((boxesCount - 5) / 2);
+                i++
+            ) {
+                boxes.push(i);
+            }
+            boxes.push(null);
+            boxes.push(pages - 1);
+        }
+        return boxes;
     }
 }
