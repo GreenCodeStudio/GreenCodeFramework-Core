c2ea989d75107d38f9af8eb158887dda29300f41
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2020-07-25 19:53:13 +0200

    i18n in js

--- js/Internationalization/i18nWebpackLoader.js
+++ js/Internationalization/i18nWebpackLoader.js
@@ -7,9 +7,10 @@ module.exports = function loader(xml) {
 import {I18nNode} from "../Core/js/Internationalization/i18nNode.js";
 import {I18nTextValue} from "../Core/js/Internationalization/i18nTextValue.js";
 import {Translator} from "../Core/js/Internationalization/translator.js";
+import {LanguagesHierarchy} from "../Core/js/Internationalization/languagesHierarchy.js";
 console.log(I18nNode);
 export const node = ${xmlToNode(doc.documentElement)};
-export const translator=new Translator(node);
+export const translator=new Translator(LanguagesHierarchy.default, node.node);
 export function t(q){return translator.translate(q).toString();}
 `;
 }
@@ -18,11 +19,11 @@ function xmlToNode(xml) {
     let childNodes = Array.from(xml.childNodes).filter(x => x.tagName == 'node').map(x => xmlToNode(x));
     let values = Array.from(xml.childNodes).filter(x => x.tagName == 'value').map(x => xmlToValue(x));
     var name = xml.getAttribute("name")
-    return `new I18nNode(${JSON.stringify(name)},[${values.join(',')}],[${childNodes.join(',')}])`;
+    return `{name:${JSON.stringify(name)}, node:new I18nNode([${values.join(',')}],[${childNodes.join(',')}])}`;
 }
 
 function xmlToValue(xml) {
     var lang = xml.getAttribute("lang")
     var value = xml.textContent;
-    return `new I18nTextValue(${JSON.stringify(lang)}, ${JSON.stringify(value)})`;
+    return `{lang:${JSON.stringify(lang)}, value: new I18nTextValue(${JSON.stringify(value)})}`;
 }
