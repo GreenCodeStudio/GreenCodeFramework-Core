72a02e5b69045db85bc170039729e2902104e4ec
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2020-11-21 21:06:11 +0100

    selection & context menu in objectlist table

--- js/ObjectsList/objectsList.js
+++ js/ObjectsList/objectsList.js
@@ -10,6 +10,9 @@ export class ObjectsList extends HTMLElement {
         this.datasource.onchange = () => this.refresh();
         this.start = 0;
         this.limit = 10
+        this.selected = new Set();
+        this.selectedMain = null;
+        this.dataById = new Map();
     }
 
     async refresh() {
@@ -19,13 +22,30 @@ export class ObjectsList extends HTMLElement {
         const refreshSymbol = Symbol();
         this.lastRefreshSymbol = refreshSymbol;
         let data = await this.datasource.get(this);
-        if (this.lastRefreshSymbol == refreshSymbol)
+        if (this.lastRefreshSymbol == refreshSymbol) {
+            this.currentRows = data.rows;
+            this.fillDataById(data.rows);
             this.insideView.loadData(data);
+        }
     }
 
     initInsideView() {
         this.insideView = new TableView(this);
         this.append(this.insideView);
+    }
+
+    fillDataById(rows) {
+        for (let row of rows) {
+            this.dataById.set(parseInt(row.id), row);
+        }
+    }
+
+    getSelectedData() {
+        let ids = [];
+        if (this.selectedMain)
+            ids.push(this.selectedMain);
+        ids = [...ids, ...Array.from(this.selected).filter(id => id != this.selectedMain)];
+        return ids.map(id => this.dataById.get(parseInt(id)));
     }
 }
 
--- js/ObjectsList/tableView.js
+++ js/ObjectsList/tableView.js
@@ -1,3 +1,6 @@
+import {ContextMenu} from "../contextMenu";
+import {pageManager} from "../pageManager";
+
 export class TableView extends HTMLElement {
     constructor(objectsList) {
         super();
@@ -49,18 +52,27 @@ export class TableView extends HTMLElement {
             td.append(column.content(data));
         }
         let actionsTd = tr.addChild('td.actions');
-        let actions = this.objectsList.generateActions([data]);
+        let actions = this.objectsList.generateActions([data], 'row');
         for (let action of actions) {
             let actionButton = actionsTd.addChild(action.href ? 'a.button' : 'button', {
                 href: action.href,
                 title: action.name
             });
             if (action.icon) {
-                actionButton.addChild('span.icon-' + action.icon);
+                actionButton.addChild('span', {classList: [action.icon]});
             } else {
                 actionButton.textContent = action.name;
             }
         }
+
+
+        tr.dataset.row = data.id;
+        tr.oncontextmenu = this.contextMenu.bind(this, tr);
+        tr.onclick = this.trOnClick.bind(this, data);
+        tr.ondblclick = this.trOnDblClick.bind(this, data, tr);
+        tr.onkeydown = this.trOnKeyDown.bind(this, data, tr);
+        tr.oncopy = this.trOnCopy.bind(this, data, tr);
+
         return tr;
     }
 
@@ -107,6 +119,143 @@ export class TableView extends HTMLElement {
         if (this.objectsList.sort)
             this.head.querySelectorAll(`[data-sort-name="${this.objectsList.sort.col}"]`).forEach(x => x.dataset.order = this.objectsList.sort.desc ? 'desc' : 'asc');
     }
+
+    contextMenu(tr, event) {
+        if (!this.objectsList.selected.has(tr.dataset.row)) {
+            this.objectsList.selected.clear();
+            this.objectsList.selected.add(tr.dataset.row)
+            this.objectsList.selectedMain = tr.dataset.row;
+            this.refreshSelectedClasses();
+        }
+        let actions = this.objectsList.generateActions(this.objectsList.getSelectedData(), 'contextMenu');
+        let elements = actions.map(action => ({
+            text: action.name,
+            icon: action.icon,
+            onclick: action.command || (() => pageManager.goto(action.href))
+        }));
+        ContextMenu.openContextMenu(event, elements);
+    }
+
+    refreshSelectedClasses() {
+        for (const tr of this.body.children) {
+            tr.classList.toggle('selected', this.objectsList.selected.has(tr.dataset.row));
+            tr.classList.toggle('selectedMain', this.objectsList.selectedMain == tr.dataset.row);
+            if (this.objectsList.selectedMain == tr.dataset.row) {
+                tr.tabIndex = 1;
+                tr.focus();
+                getSelection().selectAllChildren(tr)
+            } else {
+                tr.tabIndex = -1;
+            }
+        }
+    }
+
+
+    trOnClick(row, e) {
+        const rowsIds = this.objectsList.currentRows.map(x => x.id);
+        console.log('click')
+        if (!e.ctrlKey) {
+            this.objectsList.selected.clear();
+        }
+
+        if (e.shiftKey) {
+            const mainIndex = rowsIds.indexOf(this.objectsList.selectedMain);
+            const clickedIndex = rowsIds.indexOf(row.id);
+            if (clickedIndex >= 0 && mainIndex >= 0)
+                rowsIds.slice(Math.min(mainIndex, clickedIndex), Math.max(mainIndex, clickedIndex) + 1).forEach(x => this.objectsList.selected.add(x));
+        } else {
+            if (this.objectsList.selected.has(row.id))
+                this.objectsList.selected.delete(row.id);
+            else
+                this.objectsList.selected.add(row.id);
+        }
+
+        this.objectsList.selectedMain = row.id;
+        this.refreshSelectedClasses();
+    }
+
+    trOnDblClick(row, tr, e) {
+        if (!this.objectsList.selected.has(tr.dataset.row)) {
+            this.objectsList.selected.clear();
+            this.objectsList.selected.add(tr.dataset.row)
+            this.objectsList.selectedMain = tr.dataset.row;
+            this.refreshSelectedClasses();
+        }
+        let action = this.objectsList.generateActions(this.objectsList.getSelectedData(), 'dblClick').find(x => x.main);
+        if (action) {
+            if (action.command) {
+                action.command();
+            } else if (action.href) {
+                pageManager.goto(action.href)
+            }
+        }
+    }
+
+    selectRange(start, end) {
+        const rowsIds = this.objectsList.currentRows.map(x => x.id);
+        let startIndex = rowsIds.indexOf(start);
+        let endIndex = rowsIds.indexOf(end);
+        if (endIndex < startIndex) {
+            let tmp = startIndex;
+            startIndex = endIndex;
+            endIndex = tmp;
+        }
+        for (let i = startIndex; i <= endIndex; i++) {
+            this.objectsList.selected.add(rowsIds[i]);
+        }
+    }
+
+    trOnKeyDown(row, tr, e) {
+        console.log('trOnKeyDown')
+        if (e.key === 'Enter') {
+            const defaultButton = tr.querySelector('.button.default, .button.default');
+            if (defaultButton)
+                defaultButton.click();
+        } else if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+            const rowsIds = this.objectsList.currentRows.map(x => x.id);
+            let index = rowsIds.indexOf(this.objectsList.selectedMain);
+            if (e.key === 'ArrowDown') {
+                if (index < rowsIds.length) index++;
+                else index = rowsIds.length - 1;
+            } else {
+                if (index > 0) index--;
+            }
+            const id = rowsIds[index];
+            if (!e.ctrlKey) {
+                this.objectsList.selected.clear();
+            }
+            if (e.shiftKey) {
+                if (!this.objectsList.selectedShiftStart) {
+                    this.objectsList.selectedShiftStart = this.objectsList.selectedMain;
+                }
+                this.selectRange(this.objectsList.selectedShiftStart, id)
+            } else {
+                this.objectsList.selectedShiftStart = null;
+                if (!e.ctrlKey) {
+                    this.objectsList.selected.add(id);
+                }
+            }
+            this.objectsList.selectedMain = id;
+            this.refreshSelectedClasses();
+            e.preventDefault();
+        } else if (e.key === ' ') {
+            if (this.objectsList.selected.has(this.objectsList.selectedMain))
+                this.objectsList.selected.delete(this.objectsList.selectedMain);
+            else
+                this.objectsList.selected.add(this.objectsList.selectedMain);
+
+            this.refreshSelectedClasses();
+            e.preventDefault();
+        }
+    }
+
+    trOnCopy(row, oeyginalTr, e) {
+        const trs = Array.from(this.table.tBodies).flatMap(tbody => Array.from(tbody.children)).filter(tr => this.objectsList.selected.has(tr.dataset.row));
+        e.clipboardData.setData('text/html', '<table>' + trs.map(tr => tr.outerHTML).join('') + '</table>');
+        e.clipboardData.setData('text/plain', trs.map(tr => Array.from(tr.children).map(x => x.textContent.replace(/\r\n/gm, ' ')).join("\t")).join("\r\n"));
+        e.preventDefault();
+    }
+
 }
 
 customElements.define('table-view', TableView);
