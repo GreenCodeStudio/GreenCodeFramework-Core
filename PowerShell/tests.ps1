f8552fc0d262d4134e0aee9099187c1548747f62
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2021-08-02 15:57:57 +0200

    cicd testing

--- PowerShell/tests.ps1
+++ PowerShell/tests.ps1
@@ -2,6 +2,8 @@ function Run-UnitTests{
     try{
         Push-Location (Find-ProjectDir).Fullname
         Prepare-Build
+        composer install
+        yarn
         foreach($d in (ls ./modules/)){
             if(test-path "./modules/$d/Tests"){
                 echo "./modules/$d/Tests";
@@ -21,6 +23,7 @@ function Run-E2eTests{
     try{
         Push-Location (Find-ProjectDir).Fullname
         Run-TestEnvironment
+        node ./modules/E2eTests/Selenium/selenium.js
         Pop-Location
     }
     catch{
