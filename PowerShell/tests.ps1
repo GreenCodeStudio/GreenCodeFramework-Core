4f874ebc875b7cc53228345748be8b76be99ca5f
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2021-10-04 11:40:22 +0200

    cicd

--- PowerShell/tests.ps1
+++ PowerShell/tests.ps1
@@ -1,11 +1,15 @@
-function Run-UnitTests{
-    try{
+function Run-UnitTests
+{
+    try
+    {
         Push-Location (Find-ProjectDir).Fullname
         Prepare-Build
         composer install
         yarn
-        foreach($d in (ls ./modules/)){
-            if(test-path "./modules/$d/Tests"){
+        foreach ($d in (ls ./modules/))
+        {
+            if (test-path "./modules/$d/Tests")
+            {
                 echo "./modules/$d/Tests";
                 ./vendor/bin/phpunit "./modules/$d/Tests"
             }
@@ -13,42 +17,58 @@ function Run-UnitTests{
         #yarn test
         Pop-Location
     }
-    catch{
-      Write-Host "An error occurred:"
-      Write-Host $_
-      exit -1
+    catch
+    {
+        Write-Host "An error occurred:"
+        Write-Host $_
+        exit -1
     }
 }
-function Run-E2eTests{
-    try{
+function Run-E2eTests
+{
+    try
+    {
         Push-Location (Find-ProjectDir).Fullname
         composer install
         composer install
-        try{
-        Run-Command Migration Preview
-        }catch{
-       Write-Host $_
-       }
-       try{
-        Run-Command Migration Upgrade
-        }catch{
-       Write-Host $_
-       }
+        try
+        {
+            Run-Command Migration Preview
+        }
+        catch
+        {
+            Write-Host $_
+        }
+        try
+        {
+            Run-Command Migration Upgrade
+        }
+        catch
+        {
+            Write-Host $_
+        }
+
+        $mail = "e2etest_"+ -join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_}) + "@green-code.studio"
+        $password = -join ((65..90) + (97..122) | Get-Random -Count 20 | % {[char]$_})
+        add-User Test Admin $mail $password
+
         Run-TestEnvironment
-        node ./modules/E2eTests/Selenium/selenium.js
+        node ./modules/E2eTests/Selenium/selenium.js $mail $password
         Pop-Location
     }
-    catch{
-       Write-Host "An error occurred:"
-       Write-Host $_
-      exit -1
+    catch
+    {
+        Write-Host "An error occurred:"
+        Write-Host $_
+        exit -1
     }
 }
 
-function Run-TestEnvironment{
+function Run-TestEnvironment
+{
     Push-Location (Find-ProjectDir).Fullname
     Build-Project -Production
     cd public_html
-    Start-Job -ScriptBlock  { php -S 0.0.0.0:8080}
+    Start-Job -ScriptBlock  { php -S 0.0.0.0:8080 }
     Pop-Location
 }
