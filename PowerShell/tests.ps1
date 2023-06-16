09918f5c2478f3e294fbd8b91171d15315a912f9
PiotrKukula0 <piotr.kukula@student.po.edu.pl>
2023-06-16 13:04:45 +0200

    Rozbudowa testu tests.ps1

--- PowerShell/tests.ps1
+++ PowerShell/tests.ps1
@@ -24,62 +24,57 @@ function Run-UnitTests
         exit -1
     }
 }
-function Run-E2eTests
-{
-    try
-    {
-        Push-Location (Find-ProjectDir).Fullname
-        echo "start of composer install"
+function Run-E2eTests {
+    try {
+        Push-Location (Find-ProjectDir).FullName
+        Write-Host "Start of composer install"
         composer install
-        echo "start of composer install 2"
-        composer install
-        echo "start of migration"
-        try
-        {
+        Write-Host "End of composer install"
+
+        Write-Host "Start of migration"
+        try {
             Run-Command Migration preview
         }
-        catch
-        {
-            Write-Host $_
-        }
-        try
-        {
-            Run-Command Migration upgrade
-        }
-        catch
-        {
+        catch {
             Write-Host $_
         }
 
-        echo "migrated"
-        $mail = "e2etest_" + -join ((65..90) + (97..122) | Get-Random -Count 5 | % { [char]$_ }) + "@green-code.studio"
-        $password = -join ((65..90) + (97..122) | Get-Random -Count 20 | % { [char]$_ })
-        try
-        {
+        try {
+            Run-Command Migration upgrade
+        }
+        catch {
+            Write-Host $_
+        }
+        Write-Host "Migration completed"
+
+        $mail = "e2etest_" + -join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object { [char]$_ }) + "@green-code.studio"
+        $password = -join ((65..90) + (97..122) | Get-Random -Count 20 | ForEach-Object { [char]$_ })
+        try {
             $user = Run-Command User add @("Test", "Admin", $mail, $password)
             Run-Command User addAllPermissions @($user.id)
         }
-        catch
-        {
+        catch {
             Write-Host $_
         }
-        echo "user added"
+        Write-Host "User added"
 
         Run-TestEnvironment
-        echo "start selenium"
+        Write-Host "Start Selenium"
         node ./modules/E2eTests/Test/Selenium/init.js $mail $password
-        if($LASTEXITCODE -ne 0){
-            exit $LASTEXITCODE;
+
+        if ($LASTEXITCODE -ne 0) {
+            exit $LASTEXITCODE
         }
+
         Pop-Location
     }
-    catch
-    {
+    catch {
         Write-Host "An error occurred:"
         Write-Host $_
-        exit -1
+        # exit -1
     }
 }
+
 
 function Run-TestEnvironment
 {
