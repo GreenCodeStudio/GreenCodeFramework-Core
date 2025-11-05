42a25c491982aa94b583cea57839294d58cb77ea
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2025-11-05 14:53:36 +0100

    .

--- Repository.php
+++ Repository.php
@@ -47,4 +47,46 @@ abstract class Repository
         $defaultTable = $this->defaultTable();
         return DB::get("SELECT id, id as title FROM $defaultTable");
     }
+
+    public function generateColumnFilterSql(object $columnFilters, array $columnMap, array &$sqlParams): string
+    {
+        $ret = [];
+        foreach ((array)$columnFilters as $key => $columnFilter) {
+            if (isset($columnMap[$key])) {
+                $columnName = $columnMap[$key];
+                $paramName = "colfilter_$key";
+
+                if ($columnFilter->type == 'equals') {
+                    $ret[] = "$columnName = :$paramName";
+                    $sqlParams[$paramName] = $columnFilter->value;
+                } else if ($columnFilter->type == 'less') {
+                    $ret[] = "$columnName < :$paramName";
+                    $sqlParams[$paramName] = $columnFilter->value;
+                } else if ($columnFilter->type == 'more') {
+                    $ret[] = "$columnName > :$paramName";
+                    $sqlParams[$paramName] = $columnFilter->value;
+                } else if ($columnFilter->type == 'notEquals') {
+                    $ret[] = "$columnName != :$paramName";
+                    $sqlParams[$paramName] = $columnFilter->value;
+                } elseif ($columnFilter->type == 'contains') {
+                    $ret[] = "$columnName LIKE :$paramName";
+                    $sqlParams[$paramName] = '%'.$columnFilter->value.'%';
+                } elseif ($columnFilter->type == 'oneOf') {
+                    $ret[] = "$columnName in (:$paramName)";
+                    $sqlParams[$paramName] = $columnFilter->value;
+                }
+            }
+        }
+        if (empty($ret)) {
+            $ret[] = '1';
+        }
+        return implode(' AND ', $ret);
+    }
+
+    protected function generateOrderSQL($options, $mapping)
+    {
+        if (empty($mapping[$options->sort->col]))
+            return '';
+        return ' ORDER BY '.($mapping[$options->sort->col]).' '.($options->sort->desc ? 'DESC' : 'ASC').' ';
+    }
 }
